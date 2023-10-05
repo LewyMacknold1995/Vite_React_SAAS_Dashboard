53df4bf5915c47823b90ee4158fd4631aba6c33c
Lewy Macknold <lewy@Lewys-MacBook.local>
2023-10-05 15:09:29 +0100

    Finished Dashboard

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SalesItems from './SalesItems'
 import Webanalytics from './Webanalytics'
+import ScoreList from './ScoreList'
 
 const RightColumn = () => {
   return (
     <div className='w-full p-2'>
         <SalesItems />
         <Webanalytics />
+        <ScoreList />
     </div>
   )
 }
